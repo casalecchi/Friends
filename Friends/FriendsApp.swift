52f1d7927b4d2494e7d1013df81e3f15eb9602ba
casalecchi <felipevilela191@gmail.com>
2023-04-17 11:58:41 -0300

    Initial Commit

---
+++ Friends/FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsApp.swift
+//  Friends
+//
+//  Created by Felipe Casalecchi on 17/04/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
